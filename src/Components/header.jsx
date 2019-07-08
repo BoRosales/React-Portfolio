37c0389819c6b5f6a0ca004651ede106bfe877a5
Boni Rosales <BRosales@ufcunet.ad>
2019-07-08 10:00:44 -0500

    Finished header component

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-export default class Header extends Component {
+export class Header extends React.Component {
 render() {
     return (
         <div>
-
+            <AppBar position="static" color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" color="secondary">
+                        Bo Rosales
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </div>
         )
     }
